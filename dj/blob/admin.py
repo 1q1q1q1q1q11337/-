7377d65c1dcb66c48c084d1c6e40c6807fef564c
hipoppoltark <poltavinaliza2004@gmail.com>
2022-04-09 13:48:27 +0300

    ZZZZZZZZZZ

--- dj/blob/admin.py
+++ dj/blob/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Coment
 
 admin.site.register(Post)
-admin.site.register(Comet)
+admin.site.register(Coment)
 
